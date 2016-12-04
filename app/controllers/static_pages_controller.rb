f394e2784d4d094d68f518dd58736069e3078b8e
meaghanjones <meaghan.m.jones@gmail.com>
2016-12-03 23:54:21 -0500

    add district 7680 clubs page and preliminary informatin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing_page
   end
+
+  def district_7680_clubs
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#landing_page'
   resources :events
+  controller :static_pages do
+    get :district_7680_clubs
+end
 end
