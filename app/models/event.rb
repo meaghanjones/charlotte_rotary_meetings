b1e5041f3bf3ae802e5f26c459413a91fff17e9c
meaghanjones <meaghan.m.jones@gmail.com>
2016-12-03 20:04:12 -0500

    add form to add a new event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   validates :speaker_name, :presence => true
-  validates :speach_topic, :presence => true
+  validates :speech_topic, :presence => true
   validates :date_time, :presence => true
   validates :location, :presence => true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Event do
   it {should validate_presence_of :speaker_name}
-  it {should validate_presence_of :speach_topic}
+  it {should validate_presence_of :speech_topic}
   it {should validate_presence_of :date_time}
   it {should validate_presence_of :location}
 end
